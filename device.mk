1d19feccab846d9869c36518375d5e3fd7b15f78
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-11 16:21:54 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -9,8 +9,3 @@ LOCAL_PATH := device/samsung/gta8wifi
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
